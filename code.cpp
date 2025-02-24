d364fb5eb0adb3aba3ca1c8ba1754fb2f91f646c
gitswati-27 <swatik2706@gmail.com>
2025-02-24 20:14:50 +0530

    added the main part of the program : )

---
+++ code.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+string makePassword(int l){
+    
+}
+int main(){
+    int length;
+    cout<<"Enter minimum required length of the password : ";
+    cin>>length;
+    string password = makePassword(length);
+    cout<<"Generated password : "<<password;
+    return 0;
+}
