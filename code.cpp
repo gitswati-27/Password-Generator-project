51440dcb8340b1bd89ddea664fb1a4d73125e1b9
gitswati-27 <swatik2706@gmail.com>
2025-02-24 20:24:47 +0530

    added the actual function

--- code.cpp
+++ code.cpp
@@ -1,7 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
 string makePassword(int l){
-    
+    string Password = "";
+    string all = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890@$*";
+    int Size = all.size();
+    for(int i=0;i<l;i++){
+        int RandIndex = rand() % Size;
+        Password += all[RandIndex];
+    }
+    return Password;
 }
 int main(){
     int length;
